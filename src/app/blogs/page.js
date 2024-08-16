2abfa7c76ac5316886c471976654d779df6cf070
thecyberjerry <himanshusharma2719@gmail.com>
2024-08-16 14:02:36 +0530

    minor updates

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -143,7 +143,7 @@ export default function Index() {
                                       className="blogs__recentblogs"
                                     >
                                       <div className="blogs__recentimg">
-                                        <Image
+                                        {/* <Image
                                           src={
                                             element?.show_img?.url &&
                                             element?.show_img?.url
@@ -151,7 +151,7 @@ export default function Index() {
                                           alt=""
                                           height={100}
                                           width={100}
-                                        />
+                                        /> */}
                                       </div>
                                       <h5>
                                         {element?.heading && element?.heading}
